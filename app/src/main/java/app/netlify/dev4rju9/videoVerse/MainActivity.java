42c86e781b16dd5c55fb914fd8a3b792a4ff2cc0
4rju9 <ohinarendra3550@gmail.com>
2024-01-07 19:34:20 +0530

    Created two blank fragments: replaced toast message from the main activity with the fragments when a user selects a item suitable fragment will be shown

--- app/src/main/java/app/netlify/dev4rju9/videoVerse/MainActivity.java
+++ app/src/main/java/app/netlify/dev4rju9/videoVerse/MainActivity.java
@@ -1,6 +1,8 @@
 package app.netlify.dev4rju9.videoVerse;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
 import android.widget.Toast;
 import app.netlify.dev4rju9.videoVerse.databinding.ActivityMainBinding;
@@ -15,14 +17,34 @@ public class MainActivity extends AppCompatActivity {
 
         // For View Binding.
         binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setTheme(R.style.DeepSkyBlueNav);
         setContentView(binding.getRoot());
 
         // TODO: Code Goes Here.
+        setFragment(new VideoFragment());
 
         binding.bottomNav.setOnItemSelectedListener( item -> {
-            Toast.makeText(MainActivity.this, "Item Clicked", Toast.LENGTH_SHORT).show();
-            return true;
+
+            int itemId = item.getItemId(); // item id of clicked item from bottom navigation bar.
+
+            if (itemId == R.id.videoView) {
+                setFragment(new VideoFragment());
+                return true;
+            } else if (itemId == R.id.folderView) {
+                setFragment(new FolderFragment());
+                return true;
+            }
+            return false;
         });
+
+    }
+
+    private void setFragment (Fragment fragment) {
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragmentFL, fragment);
+        transaction.disallowAddToBackStack();
+        transaction.commit();
 
     }
 
